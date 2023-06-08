42775953789c34d292274085cd6524a84888973b
Carlos Daniel Mansilla <caarloxman@gmail.com>
2023-06-08 20:21:27 -0300

    Estructura del proyecto

---
+++ src/test/java/com/backend/IntegradorFinal/IntegradorFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.IntegradorFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
